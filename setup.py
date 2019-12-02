848b1bc0e7465712d493012a7df3b8f31c587e20
Jorge Roman Espino <jorgeromanespino@gmail.com>
2019-12-02 18:11:15 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 #
 from setuptools import setup, find_packages
 #
-setup(name="aspect", packages=find_packages())
+setup(
+    name="PyAspect", #,"aspect",
+    packages=find_packages(),
+    # Needed to silence warnings (and to be a worthwhile package)
+    url='https://github.com/jorgeromanespino/PyAspect.git',
+    author='Jorge Roman Espino',
+    author_email='jorgeromanespino@gmail.com',
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1.1',
+    # The license can be anything you like
+    license='Apache License 2.0',
+    description='Aspect Oriented Design Framework',
+)
